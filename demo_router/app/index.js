869868e57c3e7633389b5ea4ad5193b22a027c27
Toge Wu <toge@TogeMacPro.local>
2017-07-06 17:42:02 +0800

    页面跳转方式测试

--- demo_router/app/index.js
+++ demo_router/app/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import RouteMap from './router/routeMap'
 
-import { hashHistory } from 'react-router'
+import { hashHistory, browserHistory } from 'react-router'
 render(<RouteMap history={hashHistory}/>, document.getElementById('root'))
 
 
